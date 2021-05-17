59323f7568f6b0b52121e94c76f3f2495984912d
gkarthiks <github.gkarthiks@gmail.com>
2021-05-17 13:21:57 -0700

    feat: decide the run based on issue label
    Signed-off-by: gkarthiks <github.gkarthiks@gmail.com>

--- .github/assets/ISSUE_TEMPLATE/new-tweet-request.md
+++ .github/assets/ISSUE_TEMPLATE/new-tweet-request.md
@@ -2,12 +2,12 @@
 name: New tweet request
 about: Create a new tweet request
 title: "[Please change the title]"
-labels: ''
+labels: tweet
 assignees: ''
 
 ---
 
-**Provide the actual tweet content below**
+**Provide the actual tweet content between the commented colons**
 <!-- Input your tweet content exactly between the commented colons -->
 <!--::-->
 
--- index.js
+++ index.js
@@ -28,128 +28,137 @@ Date.prototype.mmddyyyy = function() {
 };
 
 try {
-    
     var eventName = github.context.eventName
-    
     core.info("Current run happened for the following trigger: "+eventName)
 
     if (eventName.startsWith("issue")) {
 
-        // Extract and create the necessary variables and values
-        // sort of initialiazition part
-        var startingParseSymbol = core.getInput("starting-parse-symbol").trim();
-        var fileNameFormat = core.getInput("file-name-format").trim();
-        var pathToSave = core.getInput("path-to-save").trim();
-        var fileNameExtension = core.getInput("file-name-extension").trim();
-        var githubToken = core.getInput('token');
-        var tweetLength = core.getInput('tweet-length');
-        
-        var issueContext = github.context.payload.issue.body;
-        var issueNumber = github.context.payload.issue.number;
-        var issueTitle = github.context.payload.issue.title;
+        // Extract the label identifier in issues that needs a tweet ci run
+        var tweetLabelId = core.getInput("tweet-label-id").trim();
+        var issueLabel = "\""+tweetLabelId+"\""
 
-        var parseSymbolFirstIndex = issueContext.indexOf(startingParseSymbol)
+        // Validate and continue if the issue is opened for a tweet
+        var strLabels = JSON.stringify(github.context.payload.issue.labels)
+        if (strLabels.includes(issueLabel)) {    
+            // Extract and create the necessary variables and values
+            // sort of initialiazition part
+            var startingParseSymbol = core.getInput("starting-parse-symbol").trim();
+            var fileNameFormat = core.getInput("file-name-format").trim();
+            var pathToSave = core.getInput("path-to-save").trim();
+            var fileNameExtension = core.getInput("file-name-extension").trim();
+            var githubToken = core.getInput('token');
+            var tweetLength = core.getInput('tweet-length');
+            
+            var issueContext = github.context.payload.issue.body;
+            var issueNumber = github.context.payload.issue.number;
+            var issueTitle = github.context.payload.issue.title;
 
-        core.info(`The parsing symbol first found @ ${parseSymbolFirstIndex}`)
+            var parseSymbolFirstIndex = issueContext.indexOf(startingParseSymbol)
 
-        if (parseSymbolFirstIndex < 0 ) {        
-            core.info("The issue "+issueNumber+" is not for creation of new tweet.")
-            core.info("Setting the continue-workflow to false.")
-            core.setOutput("continue-workflow", false)
-            exit(0)
-        }
+            core.info(`The parsing symbol first found @ ${parseSymbolFirstIndex}`)
 
-        var parseSymbolLastIndex = issueContext.lastIndexOf(startingParseSymbol)
-        var tweetContent = issueContext.substring(parseSymbolFirstIndex + startingParseSymbol.length, parseSymbolLastIndex);
-        var sanitizedTweetContent = tweetContent.replace( /[\r\n]+/gm, "" )
-
-        core.info(`
-        ===========================================================================
-        Tweet content:
-            ${sanitizedTweetContent}
-        ===========================================================================
-        `)
-
-        if (sanitizedTweetContent.length > 0)  {
-            core.info("Setting the contibue-workflow to TRUE.")
-            core.setOutput("continue-workflow", true)
-        } else {
-            core.info("The issue "+issueNumber+" is not for creation of new tweet.")
-            core.info("Setting the continue-workflow to false.")
-            core.setOutput("continue-workflow", false)
-            exit(0)
-        }
-
-        // TBD: uncomment when scheduled time is enabled.
-        // var tweetScheduleTime = issueContext.substring(issueContext.indexOf("Time:")+5, issueContext.length).trim();
-
-        var fileNameDate, completeFileName;
-        var issueTitle30Chars = issueTitle.substring(0,30);
-        var sanitizedIssueTitle = issueTitle30Chars.replace(/[^a-zA-Z0-9]/g,'-').trim().slice(0, -1);
-
-        // Validate the given timestamp is valid time and return null, 
-        // if not valid, throws an error and exits.
-        // TBD: uncomment when scheduled time is enabled.
-        // validateTimestamp(tweetScheduleTime, githubToken)
-
-        // Validates the length of the tweet content
-        if (sanitizedTweetContent.length > tweetLength) {
-            commentToIssue(
-                "Tweet content length is exceeding the permitted tweet length. Please rephrase the tweet and comment /validate to trigger the workflow again.",
-                githubToken
-            )
-            core.setFailed("Tweet content length is exceeding the permitted tweet length. Please rephrase the tweet.")
-            core.info("Setting the continue-workflow to false.")
-            core.setOutput("continue-workflow", false)
-        } else {
-            core.info(`The tweet content is ${sanitizedTweetContent}`);
-
-            // Creates the new file to be committed into the repo.
-            if (!fs.existsSync(pathToSave)){
-                fs.mkdirSync(pathToSave, { recursive: true });
+            if (parseSymbolFirstIndex < 0 ) {        
+                core.info("The issue "+issueNumber+" is not for creation of new tweet.")
+                core.info("Setting the continue-workflow to false.")
+                core.setOutput("continue-workflow", false)
+                exit(0)
             }
-        
-            core.info(`    
-                The symbol declared for parsing is ${startingParseSymbol}
-                The file name format is specified as ${fileNameFormat}
-                The file name extension is specified as ${fileNameExtension}
-                The path to save the file is specified as ${pathToSave}
-                Sanitized issue title is ${sanitizedIssueTitle}
 
-            `);
-            // TBD: uncomment when scheduled time is enabled and move into the brace above.
-            // Scheduled tweet time is ${tweetScheduleTime}
-            // TBD: remove the below assignment to tweetScheduleTime
-            var tweetScheduleTime = ""
-            if (tweetScheduleTime === "") {
-                core.info("Scheduled time is null, creating the file name with the specified format.")
-                var date = new Date();
-                if (fileNameFormat === "dd-mm-yyyy-hh-MM") {
-                    fileNameDate = date.ddmmyyyy();
-                } else if (fileNameFormat === "mm-dd-yyyy-hh-MM") {
-                    fileNameDate = date.mmddyyyy();
-                }
-                completeFileName = fileNameDate+sanitizedIssueTitle+"."+fileNameExtension
-                core.info(`File name to be saved as ${completeFileName}`)
+            var parseSymbolLastIndex = issueContext.lastIndexOf(startingParseSymbol)
+            var tweetContent = issueContext.substring(parseSymbolFirstIndex + startingParseSymbol.length, parseSymbolLastIndex);
+            var sanitizedTweetContent = tweetContent.replace( /[\r\n]+/gm, "" )
+
+            core.info(`
+            ===========================================================================
+            Tweet content:
+                ${sanitizedTweetContent}
+            ===========================================================================
+            `)
+
+            if (sanitizedTweetContent.length > 0)  {
+                core.info("Setting the contibue-workflow to TRUE.")
+                core.setOutput("continue-workflow", true)
             } else {
-                fileNameDate = new Date(tweetScheduleTime).toJSON().replace(/[^a-zA-Z0-9]/g,'-').trim().slice(0, -1)
-                completeFileName = fileNameDate+sanitizedIssueTitle+"."+fileNameExtension
-                core.info(`File name to be saved as ${completeFileName}`)
+                core.info("The issue "+issueNumber+" is not for creation of new tweet.")
+                core.info("Setting the continue-workflow to false.")
+                core.setOutput("continue-workflow", false)
+                exit(0)
             }
-            const dataFilePath = pathToSave+'/'+completeFileName;
-            fs.writeFile(dataFilePath, sanitizedTweetContent, (err) => {
-                if (err) throw err;
-            });
-        
-            core.info("Parsing done, commenting on issue.")
-        
-            commentToIssue(
-                "A new file has been created with your tweet content. Please refer the below linked PR",
-                githubToken
-            )    
+
+            // TBD: uncomment when scheduled time is enabled.
+            // var tweetScheduleTime = issueContext.substring(issueContext.indexOf("Time:")+5, issueContext.length).trim();
+
+            var fileNameDate, completeFileName;
+            var issueTitle30Chars = issueTitle.substring(0,30);
+            var sanitizedIssueTitle = issueTitle30Chars.replace(/[^a-zA-Z0-9]/g,'-').trim().slice(0, -1);
+
+            // Validate the given timestamp is valid time and return null, 
+            // if not valid, throws an error and exits.
+            // TBD: uncomment when scheduled time is enabled.
+            // validateTimestamp(tweetScheduleTime, githubToken)
+
+            // Validates the length of the tweet content
+            if (sanitizedTweetContent.length > tweetLength) {
+                commentToIssue(
+                    "Tweet content length is exceeding the permitted tweet length. Please rephrase the tweet and comment /validate to trigger the workflow again.",
+                    githubToken
+                )
+                core.setFailed("Tweet content length is exceeding the permitted tweet length. Please rephrase the tweet.")
+                core.info("Setting the continue-workflow to false.")
+                core.setOutput("continue-workflow", false)
+            } else {
+                core.info(`The tweet content is ${sanitizedTweetContent}`);
+
+                // Creates the new file to be committed into the repo.
+                if (!fs.existsSync(pathToSave)){
+                    fs.mkdirSync(pathToSave, { recursive: true });
+                }
+            
+                core.info(`    
+                    The symbol declared for parsing is ${startingParseSymbol}
+                    The file name format is specified as ${fileNameFormat}
+                    The file name extension is specified as ${fileNameExtension}
+                    The path to save the file is specified as ${pathToSave}
+                    Sanitized issue title is ${sanitizedIssueTitle}
+
+                `);
+
+                // TBD: uncomment when scheduled time is enabled and move into the brace above.
+                // Scheduled tweet time is ${tweetScheduleTime}
+                // TBD: remove the below assignment to tweetScheduleTime
+                var tweetScheduleTime = ""
+                if (tweetScheduleTime === "") {
+                    core.info("Scheduled time is null, creating the file name with the specified format.")
+                    var date = new Date();
+                    if (fileNameFormat === "dd-mm-yyyy-hh-MM") {
+                        fileNameDate = date.ddmmyyyy();
+                    } else if (fileNameFormat === "mm-dd-yyyy-hh-MM") {
+                        fileNameDate = date.mmddyyyy();
+                    }
+                    completeFileName = fileNameDate+sanitizedIssueTitle+"."+fileNameExtension
+                    core.info(`File name to be saved as ${completeFileName}`)
+                } else {
+                    fileNameDate = new Date(tweetScheduleTime).toJSON().replace(/[^a-zA-Z0-9]/g,'-').trim().slice(0, -1)
+                    completeFileName = fileNameDate+sanitizedIssueTitle+"."+fileNameExtension
+                    core.info(`File name to be saved as ${completeFileName}`)
+                }
+                const dataFilePath = pathToSave+'/'+completeFileName;
+                fs.writeFile(dataFilePath, sanitizedTweetContent, (err) => {
+                    if (err) throw err;
+                });
+            
+                core.info("Parsing done, commenting on issue.")
+            
+                commentToIssue(
+                    "A new file has been created with your tweet content. Please refer the below linked PR",
+                    githubToken
+                )    
+            }
+
+            core.setOutput("issueNumber", issueNumber);
+            console.log("yes")
         }
 
-        core.setOutput("issueNumber", issueNumber);
     } else if (eventName.startsWith("pull_request")) {
         core.info("TBD: Pull Request job")
     }
